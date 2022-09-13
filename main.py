6ab2633be4a56f22e5daf6f0031c1ec6c5b07473
TomJessel <tomhj@10.74.1.74>
2022-09-13 13:36:56 +0100

    update to corr_matrix and corr_pairplot

--- main.py
+++ main.py
@@ -10,18 +10,16 @@
 22/08/2022 13:46   tomhj      1.0         Main file
 """
 import os
-import pickle
-
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 from Experiment import load
 
 
-def corr_matrix(df: pd.DataFrame):
+def corr_matrix(df: pd.DataFrame, save_fig: bool = True):
     cols = df.columns
     plt.ion()
     fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
@@ -34,24 +32,28 @@ def corr_matrix(df: pd.DataFrame):
                 vmax=1) \
         .set(title='Correlation Matrix')
     plt.tight_layout()
-
-    path = f'{exp.test_info.dataloc}/Figures'
-    png_name = f'{path}/Test {exp.test_info.testno} - Correlation Matrix.png'
-    if not os.path.isdir(path) or not os.path.exists(path):
-        os.makedirs(path)
-    fig.savefig(png_name, dpi=200)
+    plt.show()
+    if save_fig:
+        path = f'{exp.test_info.dataloc}/Figures'
+        png_name = f'{path}/Test {exp.test_info.testno} - Correlation Matrix.png'
+        if not os.path.isdir(path) or not os.path.exists(path):
+            os.makedirs(path)
+        fig.savefig(png_name, dpi=200)
     return df.corr()
 
 
-def corr_pairplot(df: pd.DataFrame):
+def corr_pairplot(df: pd.DataFrame, save_fig: bool = True):
     sns.pairplot(df, height=1.05, aspect=1.85)
-    path = f'{exp.test_info.dataloc}/Figures'
-    png_name = f'{path}/Test {exp.test_info.testno} - Pair Plot.png'
-    plt.savefig(png_name, dpi=300)
+    if save_fig:
+        path = f'{exp.test_info.dataloc}/Figures'
+        png_name = f'{path}/Test {exp.test_info.testno} - Pair Plot.png'
+        plt.savefig(png_name, dpi=300)
+    plt.show()
 
 
 if __name__ == '__main__':
     exp = load(file='Test 5')
-    feat = exp.create_feat_df()
-    c = corr_matrix(feat)
-    corr_pairplot(feat)
+    feat = exp.features.copy()
+    feat.drop([0, 1, 23, 24])
+    c = corr_matrix(feat.drop([0, 1, 23, 24]), save_fig=False)
+    corr_pairplot(feat.drop([0, 1, 23, 24]), save_fig=False)
