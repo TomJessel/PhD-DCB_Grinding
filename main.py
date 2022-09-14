5904b443625d6b51e648f527b289aa367bd2280e
TomJessel <tomhj@10.74.1.74>
2022-09-14 17:11:37 +0100

    added low pass and trigger funcs

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import scipy
 import seaborn as sns
 import mplcursors
 from scipy.signal import hilbert
+from scipy.signal import butter, filtfilt
 
 from Experiment import load
 
@@ -93,23 +94,60 @@ def ae_hits(exp: object, s: np.array):
 def envelope_hilbert(s):
     z = hilbert(s)
     inst_amp = np.abs(z)
-    fig, ax = plt.subplots(2, 1, sharex=True)
-    ax[0].plot(s)
-    ax[0].plot(inst_amp, 'r')
-    ax[1].plot(inst_amp, 'r')
-    plt.show()
+    # fig, ax = plt.subplots(2, 1, sharex='all')
+    # ax[0].plot(s)
+    # # ax[0].plot(inst_amp, 'r')
+    # ax[1].plot(inst_amp, 'r')
+    # plt.show()
+    return inst_amp
 
-    yf = scipy.fft.fft(s)
-    xf = scipy.fft.fftfreq(len(s), 1/2000000)
-    plt.figure()
-    plt.plot(xf, np.abs(yf))
-    plt.show()
+    # yf = scipy.fft.fft(s)
+    # xf = scipy.fft.fftfreq(len(s), 1/2000000)
+    # plt.figure()
+    # plt.plot(xf, np.abs(yf))
+    # plt.show()
     # todo investigate enveloping more
 
+
+def low_pass(data, cutoff, fs, order):
+    norm_cutoff = cutoff / (0.5 * fs)
+    b, a = butter(N=order, Wn=norm_cutoff, btype='lowpass', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+def trigger(d):
+    chunk_size = 500_000
+    n_chunks = len(d)/chunk_size
+
+    chunks = np.array_split(d, n_chunks)
+    grad = [(chunki[-1] - chunki[0]) / chunk_size for chunki in chunks]
+    ind = [(i * chunk_size) + (chunk_size / 2) for i in range(len(grad))]
+    zipped = tuple(zip(ind, np.abs(grad)))
+    trig = sorted([z for z in zipped if z[1] >= 1E-6], key=lambda x: x[1], reverse=True)
+
+    t = [0, 0]
+    t[0] = (trig[0][0])
+    t_y = d[int(t[0])]
+    t[1] = int(t[0]) + np.argmax(d[int(t[0]):] < t_y)
+    return t, t_y
+
+
 if __name__ == '__main__':
+    plt.close()
     exp = load(file='Test 5')
-    sig = exp.ae.readAE(10)[10000000:15000000]
-    envelope_hilbert(sig)
+    sig = exp.ae.readAE(1)  # [0:10000000]
+    e_sig = envelope_hilbert(sig)
+    f_sig = low_pass(e_sig, 10, 2000000, 2)
+    trig_points, trig_y_val = trigger(f_sig)
+    plt.plot(f_sig)
+    plt.axvline(trig_points[0], color='r')
+    plt.axvline(trig_points[1], color='r')
+    plt.axhline(trig_y_val, color='r')
+    plt.show()
+
+
+
+    # envelope_hilbert(sig)
 
     # ae_hits(exp, sig[0:3000000])
     # feat = exp.features.copy()
