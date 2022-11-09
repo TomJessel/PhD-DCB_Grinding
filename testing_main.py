3af9593bce7bc180ba3402d20f89fd50d8f00ddf
TomJessel <tom.h.jessel@gmail.com>
2022-11-09 11:16:39 +0000

    Creation of testing_main.py for quick use to check a test

---
+++ testing_main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@File    :   testing_main.py
+@Author  :   Tom Jessel
+@Contact :   jesselt@cardiff.ac.uk
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+09/11/2022 11:08   tomhj      1.0         File for quickly setting up console for checking testing data
+"""
+
+import resources
+
+try:
+    exp = resources.load()
+except NotADirectoryError:
+    exp = resources.experiment.create_obj()
