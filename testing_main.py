263a6df7e7636d61b21101d38b2de8c83b93d994
TomJessel <tom.h.jessel@gmail.com>
2022-11-09 15:10:42 +0000

    added method to quickly check most recent ae file in obj

--- testing_main.py
+++ testing_main.py
@@ -13,15 +13,20 @@
 import resources
 
 
-def main():
+def check_ae(_exp: resources.experiment.Experiment):
+    print(f'Plotting most recent AE file...')
+    _exp.ae.plotAE(-1)
+
+
+def main() -> resources.experiment.Experiment:
     try:
         _exp = resources.load()
     except NotADirectoryError:
         _exp = resources.experiment.create_obj()
 
-    print(f'{"-"*22}TESTING EXP FILE{"-"*22}')
+    print(f'{"-" * 22}TESTING EXP FILE{"-" * 22}')
     print(_exp)
-    print('-'*60)
+    print('-' * 60)
     _exp.update()
     _exp.save()
     return _exp
@@ -29,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     exp = main()
+    check_ae(exp)
