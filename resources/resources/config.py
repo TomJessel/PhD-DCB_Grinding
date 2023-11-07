c41bccd4e0fd429cd8e0d307de6aaf63f95f532d
TomJessel <tom.h.jessel@gmail.com>
2023-11-07 09:34:30 +0000

    paths to function and test file

--- resources/resources/config.py
+++ resources/resources/config.py
@@ -15,35 +15,48 @@ from pathlib import Path
 
 # Paths
 # -----------------------------------------------------------------------------
-# TODO: Make this a function so it only gets called once 
+# TODO: Make this a function so it only gets called once
 # TODO: Test the paths on both WSL and windows
 
-# Home directory from OS and user
-PLATFORM = os.name
-if PLATFORM == 'posix':
-    HOME_DIR = Path.home()
 
-elif PLATFORM == 'nt':
-    HOME_DIR = Path.home()
-else:
-    raise OSError('OS not supported')
+def config_paths():
+    """
+    Find directory paths for the project.
 
-# Relative paths to other directories
+    Returns:
+        HOME_DIR (pathlib.Path): Home directory for the user.
+        BASE_DIR (pathlib.Path): Base directory for all data and code.
+        CODE_DIR (pathlib.Path): Code directory.
+        TB_DIR (pathlib.Path): Tensorboard directory.
+        RMS_DATA_DIR (pathlib.Path): RMS data directory.
+    """
+    # Home directory from OS and user
+    PLATFORM = os.name
+    if PLATFORM == 'posix':
+        HOME_DIR = Path.home()
 
-# Base directory for all data and code
-BASE_DIR = HOME_DIR.joinpath(r'OneDrive - Cardiff University',
-                             r'Documents',
-                             r'PHD',
-                             r'AE',
-                             )
-# Code directory
-CODE_DIR = BASE_DIR.joinpath(r'PYTHON',
-                             r'Acoustic-Emission',
-                             )
-# Tensorboard directory
-TB_DIR = BASE_DIR.joinpath(r'TensorBoard',
-                           )
-# RMS data directory
-RMS_DATA_DIR = BASE_DIR.joinpath(r'Testing',
-                                 r'RMS',
+    elif PLATFORM == 'nt':
+        HOME_DIR = Path.home()
+    else:
+        raise OSError('OS not supported')
+
+    # Relative paths to other directories
+
+    # Base directory for all data and code
+    BASE_DIR = HOME_DIR.joinpath(r'OneDrive - Cardiff University',
+                                 r'Documents',
+                                 r'PHD',
+                                 r'AE',
                                  )
+    # Code directory
+    CODE_DIR = BASE_DIR.joinpath(r'PYTHON',
+                                 r'Acoustic-Emission',
+                                 )
+    # Tensorboard directory
+    TB_DIR = BASE_DIR.joinpath(r'TensorBoard',
+                               )
+    # RMS data directory
+    RMS_DATA_DIR = BASE_DIR.joinpath(r'Testing',
+                                     r'RMS',
+                                     )
+    return HOME_DIR, BASE_DIR, CODE_DIR, TB_DIR, RMS_DATA_DIR
