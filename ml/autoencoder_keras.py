76074aa7e4ec04dcff178f1a5bfcd8130372197b
TomJessel <tom.h.jessel@gmail.com>
2023-11-09 17:07:54 +0000

    Refactor code for improved readability and performance.

--- ml/autoencoder_keras.py
+++ ml/autoencoder_keras.py
@@ -8,8 +8,11 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
 from keras.models import Model
-from keras.layers import Layer, Dense, Input, Lambda
+from keras.layers import Dense, Input, Lambda, Dropout, BatchNormalization
 from keras import backend as K
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 import resources
 
@@ -38,93 +41,100 @@ else:
 # tf.config.set_visible_devices([], 'GPU')
 
 
-class Sampling(Layer):
-    def call(self, inputs):
-        z_mean, z_log_var = inputs
-        batch = tf.shape(z_mean)[0]
-        dim = tf.shape(z_mean)[1]
-        epsilon = K.random_normal(shape=(batch, dim))
-        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+class VariationalAutoEncoder(Model):
+    def __init__(self,
+                 input_dim=300,
+                 latent_dim=16,
+                 n_size=[64, 64],
+                 r_loss_factor=1000,
+                 dropout=0.01,
+                 ):
+        super().__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.n_size = n_size
+        self.r_loss_factor = r_loss_factor
+        self.dropout = dropout
+        self.encoder = self.get_encoder(input_dim, latent_dim, n_size)
+        self.decoder = self.get_decoder(input_dim, latent_dim, n_size)
+
+    def get_encoder(self, input_dim, latent_dim, n_size):
+        inputs = Input(shape=(input_dim,), name='encoder_input')
+        e = inputs
+
+        for dim in n_size:
+            e = Dense(dim, activation='relu')(e)
+            e = BatchNormalization()(e)
+            e = Dropout(self.dropout)(e)
+
+        z_mean = Dense(latent_dim, name='z_mean')(e)
+        z_log_sigma = Dense(latent_dim, name='z_log_sigma')(e)
+
+        def sampling(args):
+            z_mean, z_log_sigma = args
+            epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim),
+                                      mean=0., stddev=1.)
+            return z_mean + K.exp(z_log_sigma / 2) * epsilon
+
+        z = Lambda(sampling)([z_mean, z_log_sigma])
+
+        # encoder mapping inputs to rthe latent space
+        encoder = Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
+        return encoder
+
+    def get_decoder(self, input_dim, latent_dim, n_size):
+        latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+        d = latent_inputs
+
+        for dim in n_size[::-1]:
+            d = Dense(dim, activation='relu')(d)
+            d = BatchNormalization()(d)
+            d = Dropout(self.dropout)(d)
+
+        outputs = Dense(input_dim, activation='sigmoid')(d)
+
+        decoder = Model(latent_inputs, outputs, name='decoder')
+        return decoder
     
-
-class Encoder(Layer):
-    def __init__(self,
-                 latent_dim=16,
-                 inter_dim=64,
-                 name='encoder',
-                 **kwargs
-                 ):
-        super().__init__(name=name, **kwargs)
-        self.dense_1 = Dense(inter_dim,
-                             activation='relu',
-                             )
-        self.dense_2 = Dense(inter_dim,
-                             activation='relu',
-                             )
-        self.dense_mean = Dense(latent_dim)
-        self.dense_log_var = Dense(latent_dim)
-        self.sampling = Sampling()
-
-    def call(self, inputs):
-        x = self.dense_1(inputs)
-        x = self.dense_2(x)
-        z_mean = self.dense_mean(x)
-        z_log_var = self.dense_log_var(x)
-        z = self.sampling((z_mean, z_log_var))
-        return z_mean, z_log_var, z
-
-
-class Decoder(Layer):
-    def __init__(self,
-                 original_dim,
-                 inter_dim=64,
-                 name='decoder',
-                 **kwargs
-                 ):
-        super().__init__(name=name, **kwargs)
-        self.dense_1 = Dense(inter_dim, activation='relu')
-        self.dense_2 = Dense(inter_dim, activation='relu')
-        self.dense_out = Dense(original_dim, activation='sigmoid')
-
-    def call(self, inputs):
-        x = self.dense_1(inputs)
-        x = self.dense_2(x)
-        return self.dense_out(x)
-
-
-class _VariationalAutoEncoder(Model):
-    def __init__(self,
-                 original_dim,
-                 latent_dim=16,
-                 inter_dim=64,
-                 name='autoencoder',
-                 **kwargs
-                 ):
-        super().__init__(name=name, **kwargs)
-        self.original_dim = original_dim
-        self.encoder = Encoder(latent_dim, inter_dim)
-        self.decoder = Decoder(original_dim, inter_dim)
-
-    def call(self, inputs):
-        z_mean, z_log_var, z = self.encoder(inputs)
-        reconstructed = self.decoder(z)
-
-        kl_loss = -0.5 * tf.reduce_mean(
-            z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1
-        )
-
+    @staticmethod
+    def reconstruction_loss(y_true, y_pred):
         reconstruction_loss = tf.losses.mean_squared_error(
-            inputs, reconstructed
+            y_true,
+            y_pred,
         )
-
-        vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
-        self.add_loss(vae_loss)
-        return reconstructed
-
-    def build_graph(self):
-        x = Input(shape=(self.original_dim,))
-        return Model(inputs=[x], outputs=self.call(x))
+        return reconstruction_loss
     
+    @staticmethod
+    def kl_loss(z_mean, z_log_sigma):
+        kl_loss = -0.5 * K.sum(
+            1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma),
+            axis=1
+        )
+        return kl_loss
+    
+    @staticmethod
+    def total_loss(reconstruction_loss, kl_loss, r_loss_factor):
+        return reconstruction_loss + kl_loss
+
+    def call(self, inputs):
+        z_mean, z_log_sigma, z = self.encoder(inputs)
+        out_decoder = self.decoder(z)
+
+        recon_loss = self.reconstruction_loss(inputs, out_decoder)
+        recon_loss *= self.input_dim
+
+        kl_loss = self.kl_loss(z_mean, z_log_sigma)
+
+        vae_loss = self.total_loss(recon_loss, kl_loss, self.r_loss_factor)
+        self.add_loss(vae_loss)
+        self.add_metric(recon_loss, name='reconstruction_loss')
+        self.add_metric(kl_loss, name='kl_loss')
+        return out_decoder
+        
+    def build_graph(self):
+        x = Input(shape=(self.input_dim,))
+        return Model(inputs=[x], outputs=self.call(x))
+
     def plot_latent_space(self, data):
         z_mean, _, _ = self.encoder(data)
         
@@ -138,70 +148,26 @@ class _VariationalAutoEncoder(Model):
         return fig, ax
 
 
-# class VariationalAutoEncoder(Model):
-#     def __init__(self, input_dim=300, latent_dim=16, n_size=[64, 64]):
-#         super().__init__()
-#         self.input_dim = input_dim
-#         self.latent_dim = latent_dim
-#         self.n_size = n_size
-#         self.encoder = self.get_encoder(input_dim, latent_dim, n_size)
-#         self.decoder = self.get_decoder(input_dim, latent_dim, n_size)
+def pre_process(data: np.array, ind: (tuple, tuple)):
+    ind_tr, ind_val = ind
 
-#     def get_encoder(self, input_dim, latent_dim, n_size):
-#         inputs = Input(shape=(input_dim,), name='encoder_input')
-#         e = inputs
+    ind_tr, ind_te = train_test_split(ind_tr,
+                                      test_size=0.2,
+                                      random_state=42,
+                                      )
+    scaler = MinMaxScaler()
+    scaler.fit(data[ind_tr, :].reshape(-1, 1))
+    data_sc = scaler.transform(data.reshape(-1, 1)).reshape(data.shape)
 
-#         for dim in n_size:
-#             e = Dense(dim, activation='relu')(e)
-
-#         z_mean = Dense(latent_dim, name='z_mean')(e)
-#         z_log_sigma = Dense(latent_dim, name='z_log_sigma')(e)
-
-#         def sampling(args):
-#             z_mean, z_log_sigma = args
-#             epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim),
-#                                       mean=0., stddev=0.1)
-#             return z_mean + K.exp(z_log_sigma) * epsilon
-
-#         z = Lambda(sampling)([z_mean, z_log_sigma])
-
-#         # encoder mapping inputs to rthe latent space
-#         encoder = Model(inputs, [z_mean, z_log_sigma, z], name='encoder')
-#         return encoder
-
-#     def get_decoder(self, input_dim, latent_dim, n_size):
-#         latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-#         d = latent_inputs
-
-#         for dim in n_size[::-1]:
-#             d = Dense(dim, activation='relu')(d)
-
-#         outputs = Dense(input_dim, activation='sigmoid')(d)
-
-#         decoder = Model(latent_inputs, outputs, name='decoder')
-#         return decoder
-
-#     def call(self, inputs):
-#         out_encoder = self.encoder(inputs)
-#         z_mean, z_log_sigma, z = out_encoder
-#         out_decoder = self.decoder(z)
-
-#         reconstruction_loss = tf.keras.metrics.mean_squared_error(
-#             inputs,
-#             out_decoder,
-#         )
-#         reconstruction_loss *= self.input_dim
-#         kl_loss = 1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma)
-#         kl_loss = K.sum(kl_loss, axis=-1)
-#         kl_loss *= -0.5
-#         vae_loss = K.mean(reconstruction_loss + kl_loss)
-#         self.add_loss(vae_loss)
-#         return out_decoder
+    x_train = data_sc[ind_tr, :]
+    x_val = data_sc[ind_val, :]
+    x_test = data_sc[ind_te, :]
+    return x_train, x_test, x_val, data_sc
 
 
 if __name__ == '__main__':
 
-    exps = ['Test 8']
+    exps = ['Test 9']
     rms = {}
     for test in exps:
         rms[test] = resources.ae.RMS(test)
@@ -217,29 +183,54 @@ if __name__ == '__main__':
         rms[test]._data = rms[test].data.apply(remove_dc, axis=0)
 
     dataset = rms[exps[0]].data.values.T
-    x_train = dataset[:140, :]
-    x_test = dataset[140:, :]
-    print(f'x_train shape: {x_train.shape}')
-    print(f'x_test shape: {x_test.shape}')
 
-    vae = _VariationalAutoEncoder(300)
+    x_train, x_test, x_val, dataset_sc = pre_process(
+        dataset,
+        (range(0, 100),
+         range(100, dataset.shape[0])
+         )
+    )
+
+    vae = VariationalAutoEncoder(x_train.shape[1],
+                                 n_size=[64, 64],
+                                 latent_dim=2,
+                                 r_loss_factor=1000,
+                                 )
+    vae.build_graph().summary()
+
     vae.compile(optimizer=keras.optimizers.Adam(),
-                metrics=[keras.metrics.RootMeanSquaredError(),
-                         keras.metrics.MeanAbsoluteError(),
-                         ],
+                # metrics=[keras.metrics.RootMeanSquaredError(),
+                #          keras.metrics.MeanAbsoluteError(),
+                #          ],
                 )
     history = vae.fit(x_train, x_train,
                       validation_data=(x_test, x_test),
                       epochs=200,
                       batch_size=32,
                       )
-    vae.build_graph().summary()
 
     fig, ax = vae.plot_latent_space(dataset)
 
     fig, ax = plt.subplots()
     ax.plot(history.history['loss'], label='Training Loss')
     ax.plot(history.history['val_loss'], label='Validation Loss')
-    plt.show()
 
-# TODO: add in pre-processing steps
+    pred = vae.predict(dataset_sc)
+
+    recon_scores = vae.reconstruction_loss(dataset_sc, pred)
+    kl_scores = vae.kl_loss(*vae.encoder(dataset_sc)[:2])
+    total_scores = vae.total_loss(recon_scores, kl_scores, vae.r_loss_factor)
+    total_scores = total_scores.numpy()
+
+    fig, ax = plt.subplots()
+    ax.scatter(range(len(recon_scores)), recon_scores)
+
+    fig, ax = plt.subplots()
+    ax.scatter(range(len(kl_scores)), kl_scores)
+
+    fig, ax = plt.subplots()
+    ax.scatter(range(len(total_scores)), total_scores)
+
+    #todo: plot scatter with runout/formerror
+
+    plt.show()
