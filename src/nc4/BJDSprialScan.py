c53d0d04a9fc9ff29025bd59dfc26e6d5e41fb18
TomJessel <tom.h.jessel@gmail.com>
2024-07-18 09:18:13 +0100

    remove pd

--- src/nc4/BJDSprialScan.py
+++ src/nc4/BJDSprialScan.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import pandas as pd
 from pathlib import Path
 from scipy.interpolate import interp1d
 from nptdms import TdmsFile
