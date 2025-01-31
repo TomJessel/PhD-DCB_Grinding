cf1d513df54f829b7ec6f44c5b0f8a2aee10d5f5
TomJessel <tom.h.jessel@gmail.com>
2025-01-31 14:12:53 +0000

    update

--- spiralTest.py
+++ spiralTest.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 
 from src import config, load
 from src.nc4.BJDSprialScan import processExpSprialScans, spiralPlotter
+from src.nc4.BJDSprialScan import NC4SpiralScan
 
 HOME_DIR, BASE_DIR, CODE_DIR, TB_DIR, RMS_DATA_DIR = config.config_paths()
 
@@ -18,18 +19,37 @@ if __name__ == "__main__":
     )
     assert SCPath.exists(), "SCurve calibration file not found."
 
-    exp = load('Test 21')
+    # exp = load('Test 25')
 
-    sprialScans = processExpSprialScans(exp,
-                                        SCPath,
-                                        nomDia=NOM_DIA,
-                                        feedrate=FEEDRATE,
-                                        rpm=RPM,
-                                        fs=FS,
-                                        yOffset=YOFFSET,
-                                        calFeedrate=CALFEEDRATE,
-                                        )
+    # sprialScans = processExpSprialScans(exp,
+    #                                     SCPath,
+    #                                     nomDia=NOM_DIA,
+    #                                     feedrate=FEEDRATE,
+    #                                     rpm=RPM,
+    #                                     fs=FS,
+    #                                     yOffset=YOFFSET,
+    #                                     calFeedrate=CALFEEDRATE,
+    #                                     )
     
-    scPlotter = spiralPlotter(sprialScans, exp.test_info.testno).plot()
+    # scPlotter = spiralPlotter(sprialScans, exp.test_info.testno).plot()
+    SCANpath = BASE_DIR.joinpath(
+        r'AE',
+        r'Testing',
+        r'24_10_08_weartest_D1.3_#1000',
+        r'Spiral Scans',
+        r'Cut_058_2024_10_08_16_24_38-Ch0-50kHz.tdms'
+    )
+    assert SCANpath.exists(), "Spiral scan file not found."
 
+    sc = NC4SpiralScan(scanPath=SCANpath,
+                       sCurvePath=SCPath,
+                       toolNomDia=NOM_DIA,
+                       zSpiralFeedrate=FEEDRATE,
+                       spindleSpeed=RPM,
+                       fs=FS,
+                       yOffset=YOFFSET,
+                       scFeedrate=CALFEEDRATE)
+    sc.plotSpiralScan(Zsection=0,
+                      vmin=0.6,
+                      vmax=0.67,)
     plt.show()
